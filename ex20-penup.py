164bef5f1112846666cde5a122cb47d7a9535025
Fernanda Gastal <54810545+fegastal@users.noreply.github.com>
2022-01-05 15:44:03 +0000

    Add files via upload

--- ex20-penup.py
+++ ex20-penup.py
@@ -1,11 +1,15 @@
 import turtle
 tartaruga = turtle.Turtle()
 tartaruga.shape("turtle")
-
 # Desativando a caneta
 tartaruga.penup()
-
 # Faça 4 vezes
 for step in range(0, 4):
     tartaruga.forward(200)
     tartaruga.right(90)
+# Ativando a caneta
+tartaruga.pendown()
+# Faça 4 vezes
+for step in range(0, 4):
+    tartaruga.forward(200)
+    tartaruga.left(90)
