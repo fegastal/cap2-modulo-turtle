817dae3ac9ccba8c1901d5d1330405fe07dbd1d3
Fernanda Gastal <54810545+fegastal@users.noreply.github.com>
2022-01-05 02:02:26 +0000

    Add files via upload

---
+++ ex14-cloning-of-a-turtle.py
@@ -0,0 +1,19 @@
+'''Exercício 14
+
+Cloning of a turtle
+
+'''
+
+import turtle
+
+t = turtle.Turtle()
+
+c = t.clone()
+t.color("blue")
+c.color("red")
+t.circle(20)
+c.circle(30)
+for i in range(40, 100, 10):
+    c.circle(i)
+
+turtle.mainloop()  
