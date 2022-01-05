b33eabe40898c26f827b7746c225765f84ee667c
Fernanda Gastal <54810545+fegastal@users.noreply.github.com>
2022-01-05 01:56:39 +0000

    Add files via upload

---
+++ ex11-customização.py
@@ -0,0 +1,21 @@
+'''Exercício 11
+
+Customizações
+
+'''
+
+import turtle
+
+# Creating turtle
+t = turtle.Turtle()
+
+t.pencolor("red")
+t.fillcolor("orange")
+t.pensize(10)
+t.speed(7)
+t.begin_fill()
+t.circle(75)
+turtle.bgcolor("blue")
+t.end_fill()
+
+turtle.mainloop()
