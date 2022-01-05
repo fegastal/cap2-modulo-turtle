b7c97eafb14ad500fe99b999f03a81bfa4c8b54b
Fernanda Gastal <54810545+fegastal@users.noreply.github.com>
2022-01-05 01:30:25 +0000

    Add files via upload

---
+++ ex5-funcao-seno2.py
@@ -0,0 +1,36 @@
+'''Exercício 5
+
+Gráfico da função seno.
+
+'''
+
+import turtle
+import math
+
+def linha(x1, y1, x2, y2):
+    turtle.up()
+    turtle.goto(x1,y1)
+    turtle.pd()
+    turtle.goto(x2,y2)
+    turtle.up()
+    turtle.hideturtle()
+    
+def grafico(tartaruga, funcao, inicio, fim, n):
+
+    tam_seg = (fim - inicio) / n
+    x = inicio
+    tartaruga.up()
+    tartaruga.goto(x, funcao(x))
+    tartaruga.down()
+
+    for conta in range(n):
+        x = x + tam_seg
+        tartaruga.goto(x, funcao(x))
+
+if __name__ == '__main__':
+    turtle.setworldcoordinates(-math.pi, -2, math.pi, 2)
+    toto = turtle.Turtle()
+    toto.pen(pensize=5, pencolor='gray')
+    grafico(toto, math.sin, -math.pi, math.pi, 50)
+    toto.hideturtle()
+    turtle.exitonclick()
