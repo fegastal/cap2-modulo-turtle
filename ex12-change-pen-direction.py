5c48d9574473cd844b7f8eb64c05f86ab3e33fcb
Fernanda Gastal <54810545+fegastal@users.noreply.github.com>
2022-01-05 01:58:56 +0000

    Add files via upload

---
+++ ex12-change-pen-direction.py
@@ -0,0 +1,24 @@
+'''Exercício 12
+
+Mudando a "pen direction"
+
+'''
+
+import turtle
+
+# Creating turtle  
+t = turtle.Turtle()
+
+t.fd(100)
+t.rt(90)
+t.penup()
+t.fd(100)
+t.rt(90)
+t.pendown()
+t.fd(100)
+t.rt(90)
+t.penup()
+t.fd(100)
+t.pendown()
+
+turtle.mainloop()
